397f4a82b5d361a39bc8224c869793f06810a2c4
ctrl::abrahamGault <108093156+abrahamGault@users.noreply.github.com>
2023-05-16 13:06:09 +0200

    Add files via upload

--- profil.py
+++ profil.py
@@ -4,23 +4,21 @@ class Profil:
 
     def selecProfil(self):
         f = open("score.csv", "r")
-        champs = f.readline()
-        lignes = f.readlines()
-        table = []
-
-        ligne1 = champs.rstrip().split(",")
-        table.append(ligne1)
-        indices = [ligne1.index("joueur"), ligne1.index("ratio_v/d"), ligne1.index("serie_victoire")]
-
+        table = [ligne.rstrip().split(",") for ligne in f]
+        
         rep = input("Si vous disposez déjà d'un profil tapez o, sinon tapez n pour créer un nouveau profil: ").strip(" ")
         
         if rep == "o":
-            pfofilNbr = input("Lequel êtes-vous ? (répondez par le nombre correspondant): ")
-            #afficher nom de profil et sélectionner avec nbr   
+            for i in table:
+                print(table.index(i), i)
+
+            pfofilNbr = input("Lequel êtes-vous ? (répondez par le nombre correspondant apparaissant devant): ")
+            #afficher nom de profil et sélectionner avec nbr
 
         f.close()
         
         if rep == "n":
+            #a refaire
             f = open("score.csv", "a")
             self.nom = input("Entrez votre nom: ")
             f.write(f"\n{self.nom}, 0, 0")
@@ -28,3 +26,6 @@ class Profil:
 
     def victoire(self):
         print(f"{self.nom} a gagné")
+
+    def defaite(self):
+        pass
